ff66b2a87647368499ec4d06eb83aba622212d50
mohamedbenb003 <mohammeds.benbouaza@gmail.com>
2024-03-03 21:09:36 +0100

    TP prog

--- Ex6.c
+++ Ex6.c
@@ -3,9 +3,11 @@
 #include<string.h>
 
 int main(){
-    char *v ,*v1 ;
+    char *v ,*v1,*v2 ;
+    v2 = (char *) malloc (30*sizeof(char));
     v = (char *) malloc (24*sizeof(char));
-    if (v==NULL){
+    v1 = (char *) malloc(22*sizeof(char));
+    if (v==NULL || v1==NULL || v2==NULL){
         puts("pas de memoire !!");
     }
     puts("entrez le verbe :");
@@ -23,12 +25,13 @@ int main(){
             puts("le verbe ne termine pas avec 'er' !!");
             exit(1);
         }else{
-            for(int i=0 ;i<strlen(v)-1;i++){
+            for(int i=0 ;i<strlen(v)-2;i++){
                 v1[i] = v[i];
             }
         }
     }
     
-    puts(v1);
+    *v2 = strcat("je " ,v1);
+    puts(v2);
     
 }
