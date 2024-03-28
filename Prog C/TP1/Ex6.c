9d878609d1716ea34075d46b746d3671e54184e0
mohamedbenb003 <mohammeds.benbouaza@gmail.com>
2024-03-28 02:01:33 +0000

    fso

--- Prog C/TP1/Ex6.c
+++ Prog C/TP1/Ex6.c
@@ -3,35 +3,54 @@
 #include<string.h>
 
 int main(){
-    char *v ,*v1,*v2 ;
-    v2 = (char *) malloc (30*sizeof(char));
-    v = (char *) malloc (24*sizeof(char));
-    v1 = (char *) malloc(22*sizeof(char));
-    if (v==NULL || v1==NULL || v2==NULL){
-        puts("pas de memoire !!");
+    int i , j ;
+    char *verb = (char *)malloc(30*sizeof(char));
+    char *cong = (char *)malloc(40*sizeof(char));
+    char *v =(char *)malloc(30*sizeof(char));
+    if(verb == NULL||cong == NULL||v == NULL){
+        puts("pas de memo !!");
     }
-    puts("entrez le verbe :");
-    gets(v);
-    int a = 0;
-    if(strlen(v)<4){
-        puts("pas un verbe !!");
+    gets(verb);
+    if(strlen(verb)<4){
+        puts("pas un verb !");
         exit(1);
     }
-    if (v[strlen(v)-2] != 'e' ){
-        puts("le verbe ne termine pas avec 'er' !!");
-        exit(1);
-    }else{
-        if(v[strlen(v)-1] != 'r'){
-            puts("le verbe ne termine pas avec 'er' !!");
-            exit(1);
-        }else{
-            for(int i=0 ;i<strlen(v)-2;i++){
-                v1[i] = v[i];
-            }
+    cong[0] ='\0';
+    v[0]='\0';
+    if(verb[strlen(verb)-2] == 'e' && verb[strlen(verb)-1] == 'r'){
+        for(i=0 ; i<strlen(verb)-1 ; i++){
+            v[i] = verb[i];
         }
+        v[i]='\0';
+    }else{
+        puts("pas un verb du 1er groupe !!");
+        exit(1);
     }
-    
-    *v2 = strcat("je " ,v1);
-    puts(v2);
-    
+    strcat(cong , "je ");
+    strcat(cong ,v);
+    puts(cong);
+    cong[0] ='\0';
+    strcat(cong , "tu ");
+    strcat(cong ,v);
+    strcat(cong ,"s");
+    puts(cong);
+    cong[0] ='\0';
+    strcat(cong , "il ");
+    strcat(cong ,v);
+    puts(cong);
+    cong[0] ='\0';
+    strcat(cong , "nous ");
+    strcat(cong ,v);
+    strcat(cong ,"ons");
+    puts(cong);
+    cong[0] ='\0';
+    strcat(cong , "vous ");
+    strcat(cong ,v);
+    strcat(cong ,"z");
+    puts(cong);
+    cong[0] ='\0';
+    strcat(cong , "ils ");
+    strcat(cong ,v);
+    strcat(cong ,"nt");
+    puts(cong);
 }
